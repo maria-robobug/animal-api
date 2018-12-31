e097b959b744748b12b9f7bbf61e91d428bbbb26
maria-robobug <mariashow2001@gmail.com>
2018-12-31 17:03:26 +0000

    Refactored response body for server

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -7,6 +7,15 @@ import (
 	"runtime/debug"
 )
 
+type response struct {
+	ImageURL    string `json:"image_url"`
+	Name        string `json:"name"`
+	Height      string `json:"height"`
+	Weight      string `json:"weight"`
+	Lifespan    string `json:"lifespan"`
+	Temperament string `json:"temperament"`
+}
+
 func (app *application) getRandomDog(w http.ResponseWriter, r *http.Request) {
 	dogInfo, err := app.client.GetRandomDogInfo()
 	if err != nil {
@@ -20,5 +29,16 @@ func (app *application) getRandomDog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	json.NewEncoder(w).Encode(dogInfo[0])
+	dogImage := dogInfo[0].URL
+	dog := dogInfo[0].Breeds[0]
+	resp := &response{
+		ImageURL:    dogImage,
+		Name:        dog.Name,
+		Height:      dog.Height.Metric,
+		Weight:      dog.Weight.Metric,
+		Lifespan:    dog.LifeSpan,
+		Temperament: dog.Temperament,
+	}
+
+	json.NewEncoder(w).Encode(resp)
 }
