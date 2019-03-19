756710eb87f1a2d13dacf8d359b73a28b8d2903f
maria-robobug <mariashow2001@gmail.com>
2019-03-19 19:31:15 +0000

    Removing google cloud config for now

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const (
 )
 
 func main() {
-	// logging configuration
+	// Logging configuration
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
